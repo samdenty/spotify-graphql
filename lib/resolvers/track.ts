62e2ab63b4cd3c97bdf891a04fb89dbca41ab3ca
Sam Denty <samddenty+gh@gmail.com>
2019-04-17 19:25:11 +0100

    Fix

--- lib/resolvers/track.ts
+++ lib/resolvers/track.ts
@@ -1,71 +1,74 @@
-import { safeApiCall, syncedPoll } from '../utils';
-import * as _ from 'lodash';
-const sequence: any = require('when/sequence');
+import { safeApiCall, syncedPoll } from '../utils'
+import * as _ from 'lodash'
+const sequence: any = require('when/sequence')
 
 export function trackResolvers(spotifyApiClient) {
   return {
-    saved(track) {
-      const results = safeApiCall(
-        spotifyApiClient,
-        'containsMySavedTracks',
-        null,
-        [track.id]
-      )
-      return results ? results[0] : false;
-    }
+    async saved(track) {
+      const { body } = await spotifyApiClient.containsMySavedTracks([track.id])
+      return body[0]
+    },
     artists(track: any, variables: any) {
       if (!!variables.full) {
-        return syncedPoll('Track.artists', () => {
+        return syncedPoll(
+          'Track.artists',
+          () => {
             // This part is hacky.
             //  Since Spotify Web API do not provide album
             //  /track/:id/artists, we need to call
             //  /artists?ids=... by chunk of 50 ids.
             return new Promise((resolve, reject) => {
-              let queries: any = _(track.artists).
-                map('id').
-                compact().
-                chunk(50).
-                map((idsToQuery: any[]) => {
+              let queries: any = _(track.artists)
+                .map('id')
+                .compact()
+                .chunk(50)
+                .map((idsToQuery: any[]) => {
                   return (): Promise<any> => {
                     return safeApiCall(
                       spotifyApiClient,
                       'getArtists',
-                      (response) => response.body.artists,
+                      response => response.body.artists,
                       idsToQuery
-                    );
-                  };
-                });
+                    )
+                  }
+                })
               sequence(Array.from(queries)).then(
-                (results) => {
-                  resolve(_(results).flatten());
+                results => {
+                  resolve(_(results).flatten())
                 },
-                (e) => {
-                  reject(e);
+                e => {
+                  reject(e)
                 }
-              );
-            });
-        }, variables.throttle || 5);
+              )
+            })
+          },
+          variables.throttle || 5
+        )
       } else {
-        return track.artists;
+        return track.artists
       }
     },
     // an artist can have a large amount of albums,
     //   so we use `limitConcurency()` helper to avoid
     //   massive API calls at once
     album(track, variables) {
-      if(!!variables.full) {
-        return syncedPoll('Track.album', () => {
-          return safeApiCall(
-            spotifyApiClient,
-            'getAlbum',
-            null,
-            track.album.id
-          ).then( (album) => {
-            return album;
-          });
-        }, variables.throttle || 5);
+      if (!!variables.full) {
+        return syncedPoll(
+          'Track.album',
+          () => {
+            return safeApiCall(
+              spotifyApiClient,
+              'getAlbum',
+              null,
+              track.album.id
+            ).then(album => {
+              return album
+            })
+          },
+          variables.throttle || 5
+        )
       } else {
-        return Promise.resolve(track.album);
+        return Promise.resolve(track.album)
       }
     },
 
@@ -75,7 +78,7 @@ export function trackResolvers(spotifyApiClient) {
         'getAudioFeaturesForTrack',
         null,
         track.id
-      );
+      )
     }
-  };
+  }
 }
